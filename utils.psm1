85359db374e3369ce2ff06b626b6a9a367dfee31
Saboti <fastjack_18@gmx.de>
2018-08-29 13:23:58 +0200

    reverted disable Monitor

--- utils.psm1
+++ utils.psm1
@@ -83,10 +83,6 @@ function Disable-Devices {
     Write-Output "Disabling Hyper-V Video"
     Import-Module "$PSScriptRoot\$extract_folder\DeviceManagement.psd1"
     Get-Device | Where-Object -Property Name -Like "Microsoft Hyper-V Video" | Disable-Device -Confirm:$false
-
-    Write-Output "Disabling Generic Non-PnP Monitor"
-    Import-Module "$PSScriptRoot\$extract_folder\DeviceManagement.psd1"
-    Get-Device | Where-Object -Property Name -Like "Generic Non-PnP Monitor" | Disable-Device -Confirm:$false
 }
 
 function Enable-Audio {
